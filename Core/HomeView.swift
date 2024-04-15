1cd8df526fedf01925b150b2885f6ce79c7bc11e
Fatih Eren Özçelik <f.eren.ozcelik@gmail.com>
2024-04-15 22:49:45 +0300

    HomeView header added

--- Core/HomeView.swift
+++ Core/HomeView.swift
@@ -11,12 +11,37 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             Color.netflixBlack.ignoresSafeArea()
-            Text("Netflix")
-                .foregroundStyle(.netflixRed)
+            
+            VStack(spacing: 0) {
+                header
+                    .padding(.horizontal, 16)
+                Spacer()
+            }
         }
+        .foregroundStyle(.netflixWhite)
     }
 }
 
 #Preview {
     HomeView()
+}
+
+private extension HomeView {
+    
+    var header: some View {
+        HStack(spacing: 0) {
+            Text("For You")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title)
+            
+            HStack(spacing: 16) {
+                Image(systemName: "tv.badge.wifi")
+                    .onTapGesture { }
+                
+                Image(systemName: "magnifyingglass")
+                    .onTapGesture { }
+            }
+            .font(.title2)
+        }
+    }
 }
